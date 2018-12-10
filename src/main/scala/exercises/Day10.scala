c3154366572f453e1400d309697c84bef37eaa1c
Adam Coales <adam.coales@basementcrowd.com>
2018-12-10 10:40:05 +0000

    Day 10, no stray points

---
+++ src/main/scala/exercises/Day10.scala
@@ -0,0 +1,55 @@
+package exercises
+
+import scala.collection.mutable
+
+object Day10 {
+
+  case class Point(x: Int, y: Int)
+
+  case class Velocity(vx: Int, vy: Int)
+
+  def parseInput(seq: Seq[String]): Set[(Point, Velocity)] = {
+    val regex = """position=< ?(-?\d+),  ?(-?\d+)> velocity=< ?(-?\d+),  ?(-?\d+)>""".r
+    seq.map {
+      case regex(x, y, vx, vy) => Point(y.toInt, x.toInt) -> Velocity(vy.toInt, vx.toInt)
+    }.toSet
+  }
+
+  private def hasAdjacent(point: Point, points: Set[Point]): Boolean = {
+    val adjacents = for (x <- -1 to 1; y <- -1 to 1) yield Point(point.x + x, point.y + y)
+    adjacents.count(points.contains) > 1
+  }
+
+  @annotation.tailrec
+  def findWord(points: Set[(Point, Velocity)]): Set[Point] = {
+    val pointKeys = points.map(_._1)
+    if (pointKeys.forall(p => hasAdjacent(p, pointKeys))) {
+      pointKeys
+    } else {
+      findWord(points.map { case (p, v) => Point(p.x + v.vx, p.y + v.vy) -> v })
+    }
+  }
+
+  @annotation.tailrec
+  def timeWordFind(points: Set[(Point, Velocity)], time: Int): Int = {
+    val pointKeys = points.map(_._1)
+    if (pointKeys.forall(p => hasAdjacent(p, pointKeys))) {
+      time
+    } else {
+      timeWordFind(points.map { case (p, v) => Point(p.x + v.vx, p.y + v.vy) -> v }, time + 1)
+    }
+  }
+
+  def display(points: Set[Point]) = {
+    val minX = points.minBy(_.x).x
+    val minY = points.minBy(_.y).y
+    val maxX = points.maxBy(_.x).x
+    val maxY = points.maxBy(_.y).y
+    val grid = mutable.ArrayBuffer.fill(1 + maxX - minX, 1 + maxY - minY)(0)
+    points.foreach(p => grid(p.x - minX)(p.y - minY) = 1)
+    println("Display:")
+    println(grid.map(_.map(i => if (i == 0) "." else "#").mkString).mkString("\n"))
+    println()
+  }
+
+}
--- src/main/scala/util/ExerciseRunner.scala
+++ src/main/scala/util/ExerciseRunner.scala
@@ -10,9 +10,40 @@ object ExerciseRunner extends App {
     Source.fromResource(src).getLines
   }
 
-  val lines = readFile("day9.txt").toList
-  val rules = Day9.parseInput(lines.head)
-  println(Day9.highestScore(rules))
-  println(Day9.highestScoreVector(rules))
-  println(Day9.highestScore(rules.copy(marbles = rules.marbles * 100)))
+  val lines = readFile("day10.txt").toList
+//  val lines = """position=< 9,  1> velocity=< 0,  2>
+//                |position=< 7,  0> velocity=<-1,  0>
+//                |position=< 3, -2> velocity=<-1,  1>
+//                |position=< 6, 10> velocity=<-2, -1>
+//                |position=< 2, -4> velocity=< 2,  2>
+//                |position=<-6, 10> velocity=< 2, -2>
+//                |position=< 1,  8> velocity=< 1, -1>
+//                |position=< 1,  7> velocity=< 1,  0>
+//                |position=<-3, 11> velocity=< 1, -2>
+//                |position=< 7,  6> velocity=<-1, -1>
+//                |position=<-2,  3> velocity=< 1,  0>
+//                |position=<-4,  3> velocity=< 2,  0>
+//                |position=<10, -3> velocity=<-1,  1>
+//                |position=< 5, 11> velocity=< 1, -2>
+//                |position=< 4,  7> velocity=< 0, -1>
+//                |position=< 8, -2> velocity=< 0,  1>
+//                |position=<15,  0> velocity=<-2,  0>
+//                |position=< 1,  6> velocity=< 1,  0>
+//                |position=< 8,  9> velocity=< 0, -1>
+//                |position=< 3,  3> velocity=<-1,  1>
+//                |position=< 0,  5> velocity=< 0, -1>
+//                |position=<-2,  2> velocity=< 2,  0>
+//                |position=< 5, -2> velocity=< 1,  2>
+//                |position=< 1,  4> velocity=< 2,  1>
+//                |position=<-2,  7> velocity=< 2, -2>
+//                |position=< 3,  6> velocity=<-1, -1>
+//                |position=< 5,  0> velocity=< 1,  0>
+//                |position=<-6,  0> velocity=< 2,  0>
+//                |position=< 5,  9> velocity=< 1, -2>
+//                |position=<14,  7> velocity=<-2,  0>
+//                |position=<-3,  6> velocity=< 2, -1>""".stripMargin.split("\n")
+  val points = Day10.parseInput(lines)
+  val wordPoints = Day10.findWord(points)
+  Day10.display(wordPoints)
+  println(Day10.timeWordFind(points, 0))
 }
